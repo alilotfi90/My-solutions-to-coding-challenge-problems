35e4d3d1e9e2a138f720bb3d5cb68363567e809a
Ali Lotfi <43100404+alilotfi90@users.noreply.github.com>
2021-09-29 16:40:23 -0600

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  linked_list.py
+#  
+#  Copyright 2021 Ali Lotfi <ali@ali-Oryx-Pro>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.s
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import numpy as np
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+def main(args):
+	n=Node(2)
+	m=Node(3)
+	#print(n.dataval)
+	s=SLinkedList()
+	s.headval=n
+	n.nextval=m
+	s.listprint()
+	
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
